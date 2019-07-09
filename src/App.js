76bb255971dbf4c4e45435b82d61a6346522915f
Yasuhiro Imo <yasuhiro.imo@pol.co.jp>
2019-07-09 14:58:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={name: 'James'}
+  }
+  
+  render() {
+    const name=this.state.name;
+
+    return (
+      <div>
+        <h1>Hello, {name}.</h1>
+        <input
+          type="text"
+          value={name}
+          onChange={ e => {
+            this.setState({
+              name: e.target.value
+            })
+          }}
+        />
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save tototo reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from'./App';
+// import * as serviceWorker from './serviceWorker';
+// serviceWorker.register();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+
+// class Board extends React.Component {
+//     renderSquare(i) {
+//         return <Square value={i} />;
+//     }
+// }
+
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square">
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
